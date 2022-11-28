2229aa4ff4cd09a7e6d6ff430bb1524c77b97155
Kirill <kirillbronski@gmail.com>
2022-11-28 10:15:33 +0300

    001_rows_columns: column

--- app/src/main/java/com/bronski/android/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/android/compose/MainActivity.kt
@@ -3,15 +3,20 @@ package com.bronski.android.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
+            Column(
+                modifier = Modifier.background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally) {
                 Text("Hello")
                 Text("World")
             }
