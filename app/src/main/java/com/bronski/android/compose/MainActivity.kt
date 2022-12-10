92ac1cc74ce0b3944c3db2ebcfcd0554853be737
Kirill <kirillbronski@gmail.com>
2022-12-10 16:46:22 +0300

    002_modifier: play with borders

--- app/src/main/java/com/bronski/android/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/android/compose/MainActivity.kt
@@ -3,38 +3,46 @@ package com.bronski.android.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import com.bronski.android.compose.ui.theme.ComposeTheme
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+
+            ){
+                Text("Hello", modifier = Modifier
+                    .offset(20.dp,20.dp)
+                    .border(5.dp, Color.Yellow)
+                    .padding(5.dp)
+                    .border(10.dp, Color(R.color.purple_200))
+                    .padding(10.dp)
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text("Compose", color = Color.Yellow)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Android")
     }
 }
