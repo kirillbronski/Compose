820f929efc42baf14752c7c1f1264a1dc9adce09
Kirill <kirillbronski@gmail.com>
2023-02-12 18:43:42 +0300

    part_14_meditation_ui: implemented GreetingSection, ChipSection

--- app/src/main/java/com/bronski/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/compose/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bronski.compose.ui.HomeScreen
 import com.bronski.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTheme {
-
+                HomeScreen()
             }
         }
     }
