5e73e07285be9da4dec3509083248ad1e98a2d2e
Kirill <kirillbronski@gmail.com>
2023-02-20 23:20:50 +0300

    part_14_current meditation - 38:10

--- app/src/main/java/com/bronski/compose/ui/HomeScreen.kt
+++ app/src/main/java/com/bronski/compose/ui/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.bronski.compose.ui
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -18,13 +19,16 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bronski.compose.R
 import com.bronski.compose.data.local.Feature
 import com.bronski.compose.ui.theme.*
+import com.bronski.compose.util.standardQuadFromTo
 
 @Composable
 fun HomeScreen() {
@@ -184,6 +188,58 @@ fun FeatureItem(
         val width = constraints.maxWidth
         val height = constraints.maxHeight
 
+        // Medium colored path
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+//            quadraticBezierTo(
+//                mediumColoredPoint2.x,
+//                mediumColoredPoint2.y,
+//                (mediumColoredPoint1.x + mediumColoredPoint2.x) / 2f,
+//                (mediumColoredPoint1.y + mediumColoredPoint2.y) / 2f,
+//            )
+            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+
+        // Medium colored path
+        val lightPoint1 = Offset(0f, height * 0.35f)
+        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+
+        // Light colored path
+        val lightColoredPath = Path().apply {
+            moveTo(lightPoint1.x, lightPoint1.y)
+            standardQuadFromTo(lightPoint1, lightPoint2)
+            standardQuadFromTo(lightPoint2, lightPoint3)
+            standardQuadFromTo(lightPoint3, lightPoint4)
+            standardQuadFromTo(lightPoint4, lightPoint5)
+
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            drawPath(path = mediumColoredPath, color = feature.mediumColor)
+            drawPath(path = lightColoredPath, color = feature.lightColor)
+        }
 
     }
 }
