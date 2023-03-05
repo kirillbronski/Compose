bde2b2a8b5a1393842c66a0e84403374764a7419
Kirill <kirillbronski@gmail.com>
2023-03-05 14:44:55 +0300

    part_17_instagram: profile section

--- app/src/main/java/com/bronski/compose/ui/profileScreen/ProfileScreen.kt
+++ app/src/main/java/com/bronski/compose/ui/profileScreen/ProfileScreen.kt
@@ -1,7 +1,9 @@
 package com.bronski.compose.ui.profileScreen
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -10,7 +12,9 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -23,6 +27,8 @@ fun ProfileScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
         TopBar(name = "Kirill Bronski")
         Spacer(modifier = Modifier.height(4.dp))
+        ProfileSection()
+
     }
 }
 
@@ -34,7 +40,9 @@ fun TopBar(
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround,
-        modifier = modifier.fillMaxWidth().padding(vertical = 16.dp)
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
     ) {
         Icon(
             imageVector = Icons.Default.ArrowBack,
@@ -50,16 +58,83 @@ fun TopBar(
         )
         Icon(
             painter = painterResource(id = R.drawable.baseline_notifications_none_24),
-            contentDescription = "Back",
+            contentDescription = "Bells",
             tint = Color.Black,
             modifier = Modifier.size(24.dp)
         )
         Icon(
             imageVector = Icons.Default.MoreVert,
-            contentDescription = "Back",
+            contentDescription = "More",
             tint = Color.Black,
             modifier = Modifier.size(24.dp)
         )
+    }
+}
+
+@Composable
+fun ProfileSection() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+            RoundImage(
+                image = painterResource(id = R.drawable.photo),
+                modifier = Modifier
+                    .size(100.dp)
+                    .weight(3f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            StatSection(modifier = Modifier.weight(7f))
+        }
+    }
+}
+
+@Composable
+fun RoundImage(
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(ratio = 1f, matchHeightConstraintsFirst = true)
+            .border(width = 1.dp, color = Color.LightGray, shape = CircleShape)
+            .padding(3.dp)
+            .clip(shape = CircleShape)
+    )
+}
+
+@Composable
+fun StatSection(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ) {
+        ProfileStat(numberText = "19", text = "Posts")
+        ProfileStat(numberText = "180", text = "Follower")
+        ProfileStat(numberText = "115", text = "Following")
+    }
+}
+
+@Composable
+fun ProfileStat(
+    numberText: String,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(text = numberText, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = text)
     }
 }
 
