9d74080b7b4fb09812b9923cc7f9c2c17c2a2971
Kirill <kirillbronski@gmail.com>
2023-03-20 23:34:02 +0300

    part_25_clean_theming: 4:24

--- app/src/main/java/com/bronski/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/compose/MainActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.bronski.compose.ui.LocalSpacing
+import com.bronski.compose.ui.spacing
 import com.bronski.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Surface(
+                color = MaterialTheme.colors.background,
+                modifier = Modifier.padding(MaterialTheme.spacing.medium)
+            ) {
 
+            }
         }
     }
 }
