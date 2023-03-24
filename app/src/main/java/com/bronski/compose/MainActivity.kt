3b8a37b8c4236447981d4a70a25b263e73151ebf
Kirill <kirillbronski@gmail.com>
2023-03-25 00:12:41 +0300

    part_27_all_screen_sizes

--- app/src/main/java/com/bronski/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/compose/MainActivity.kt
@@ -3,20 +3,88 @@ package com.bronski.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bronski.compose.ui.RememberWindowInfo
+import com.bronski.compose.ui.WindowInfo
 import com.bronski.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val windowInfo = RememberWindowInfo()
+            if (windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(count = 10) {
+                        Text(
+                            text = "item $it",
+                            fontSize = 25.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.Cyan)
+                                .padding(16.dp)
+                        )
+                    }
 
+                    items(count = 10) {
+                        Text(
+                            text = "item $it",
+                            fontSize = 25.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.Green)
+                                .padding(16.dp)
+                        )
+                    }
+                }
+            } else {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    LazyColumn(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        items(count = 10) {
+                            Text(
+                                text = "item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Green)
+                                    .padding(16.dp)
+                            )
+                        }
+                    }
+                    LazyColumn(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        items(count = 10) {
+                            Text(
+                                text = "item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Cyan)
+                                    .padding(16.dp)
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
