01df05489aecd0f4669532549825c31eb53df23c
Kirill <kirillbronski@gmail.com>
2023-04-08 23:14:49 +0300

    part_37_lazy_staggered_grid

---
+++ app/src/main/java/com/bronski/compose/ListItem.kt
@@ -0,0 +1,9 @@
+package com.bronski.compose
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+data class ListItem(
+    val height: Dp,
+    val color: Color
+)
--- app/src/main/java/com/bronski/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/compose/MainActivity.kt
@@ -1,28 +1,69 @@
 package com.bronski.compose
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bronski.compose.ui.theme.ComposeTheme
 import com.google.accompanist.swiperefresh.SwipeRefresh
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val items = (1..100).map {
+            ListItem(
+                height = Random.nextInt(100, 300).dp,
+                color = Color(
+                    Random.nextLong(0xFFFFFFFF)
+                ).copy(alpha = 1f)
+            )
+        }
         setContent {
             ComposeTheme {
-
+                LazyVerticalStaggeredGrid(
+                    columns = StaggeredGridCells.Fixed(2),
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalItemSpacing = 16.dp
+                ) {
+                    items(items) {
+                        RandomColorBox(item = it)
+                    }
+                }
             }
         }
     }
+}
+
+@Composable
+fun RandomColorBox(item: ListItem) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(item.height)
+            .clip(RoundedCornerShape(10.dp))
+            .background(item.color)
+    )
 }
 
 @Composable
