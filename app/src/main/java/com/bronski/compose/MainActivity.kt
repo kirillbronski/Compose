730771ddbb230b42f022f4bb9cc079191b69852b
Kirill <kirillbronski@gmail.com>
2023-04-09 19:02:10 +0300

    part_39_shimmer_loading_effect

--- app/src/main/java/com/bronski/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/compose/MainActivity.kt
@@ -3,13 +3,23 @@ package com.bronski.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.bronski.compose.components.ShimmerListItem
 import com.bronski.compose.ui.theme.ComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import net.datafaker.Faker
 import javax.inject.Inject
 
@@ -23,7 +33,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTheme {
-                Greeting(name = faker.name().firstName(), image = faker.avatar().image())
+                var isLoading by remember {
+                    mutableStateOf(true)
+                }
+                LaunchedEffect(key1 = true) {
+                    delay(10000)
+                    isLoading = false
+                }
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(20) {
+                        ShimmerListItem(
+                            isLoading = isLoading,
+                            contentAfterLoading = {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp)
+                                ) {
+                                    AsyncImage(
+                                        model = faker.avatar().image(),
+                                        contentDescription = "Home",
+                                        modifier = Modifier.size(100.dp)
+                                    )
+                                    Spacer(modifier = Modifier.width(16.dp))
+                                    Text(text = faker.bigBangTheory().quote(), maxLines = 3)
+                                }
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                }
             }
         }
     }
