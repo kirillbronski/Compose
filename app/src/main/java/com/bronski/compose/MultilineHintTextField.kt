e0c58779f085c4e0daac0b91e36410f95e7db27b
Kirill <kirillbronski@gmail.com>
2023-04-20 23:56:12 +0300

    part_22_multi_select_lazy_column: - finish

---
+++ app/src/main/java/com/bronski/compose/MultilineHintTextField.kt
@@ -0,0 +1,14 @@
+package com.bronski.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MultilineHintTextField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    hintText: String = ""
+) {
+
+}
