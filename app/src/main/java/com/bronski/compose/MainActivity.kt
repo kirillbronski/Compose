cd548d0d017ba95aec8d5a11d433dab6c051d294
Kirill <kirillbronski@gmail.com>
2023-04-27 23:21:20 +0300

    part_45_save_restore_scroll_position:

--- app/src/main/java/com/bronski/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/compose/MainActivity.kt
@@ -3,43 +3,79 @@ package com.bronski.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.bronski.compose.ui.theme.ComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
 import net.datafaker.Faker
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val prefs by lazy {
+        applicationContext.getSharedPreferences("prefs", MODE_PRIVATE)
+    }
+
     @Inject
     lateinit var faker: Faker
 
+    @OptIn(FlowPreview::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val scrollPosition = prefs.getInt("scroll_position", 0)
         setContent {
             ComposeTheme {
-                Greeting(name = faker.name().firstName(), image = faker.avatar().image())
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val lazyListState = rememberLazyListState(
+                        initialFirstVisibleItemIndex = scrollPosition
+                    )
+
+                    LaunchedEffect(key1 = lazyListState) {
+                        snapshotFlow {
+                            lazyListState.firstVisibleItemIndex
+                        }.debounce(500L)
+                            .collectLatest {
+                                prefs.edit()
+                                    .putInt("scroll_position", it)
+                                    .apply()
+                            }
+                    }
+
+                    LazyColumn(
+                        state = lazyListState,
+                        modifier = Modifier.fillMaxSize(),
+                        contentPadding = PaddingValues(16.dp)
+                    ) {
+                        items(100) {
+                            Text(
+                                text = "Item $it",
+                                modifier = Modifier.padding(16.dp)
+                            )
+                        }
+                    }
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, image: String) {
-    Text(text = "Hello $name!", textAlign = TextAlign.Center)
-    AsyncImage(model = image, contentDescription = "Avatar")
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        //Greeting("Android")
     }
 }
