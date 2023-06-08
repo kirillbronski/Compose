232f08592b104818e40bc264552f23a50b345193
Kirill <kirillbronski@gmail.com>
2023-06-08 23:34:41 +0300

    part_51_navigate_back_with_result:

--- app/src/main/java/com/bronski/compose/MainActivity.kt
+++ app/src/main/java/com/bronski/compose/MainActivity.kt
@@ -3,10 +3,25 @@ package com.bronski.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.bronski.compose.ui.theme.ComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,7 +38,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTheme {
-
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "screen1"
+                ) {
+                    composable("screen1") {
+                        val text = it.savedStateHandle.get<String>("my_text")
+                        Column(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            text?.let {
+                                 Text(text = text)
+                            }
+                            Button(onClick = {
+                                navController.navigate("screen2")
+                            }) {
+                                Text(text = "Go to screen2")
+                            }
+                        }
+                    }
+                    composable("screen2") {
+                        Column(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            var text by remember {
+                                mutableStateOf("")
+                            }
+                            OutlinedTextField(
+                                value = text,
+                                onValueChange = {
+                                    text = it
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            )
+                            Button(onClick = {
+                                navController.previousBackStackEntry
+                                    ?.savedStateHandle
+                                    ?.set("my_text", text)
+                                navController.popBackStack()
+                            }) {
+                                Text(text = "Apply")
+                            }
+                        }
+                    }
+                }
             }
         }
     }
