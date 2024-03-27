b457e0ed019fa25cfd76c29bb46bab681eb80e17
Kirill <kirillbronski@gmail.com>
2024-03-27 22:26:27 +0300

    pull to refresh

---
+++ pull-refresh-material3/src/main/java/com/example/pullrefreshmaterial3/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.pullrefreshmaterial3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pullrefreshmaterial3.ui.theme.ComposeTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            ComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val items = remember {
+                        (1..100).map { "Item $it" }
+                    }
+                    var isRefreshing by remember {
+                        mutableStateOf(false)
+                    }
+                    val scope = rememberCoroutineScope()
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        PullToRefreshLazyColumn(
+                            items = items,
+                            content = { itemTitle ->
+                                Text(
+                                    text = itemTitle,
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                )
+                            },
+                            isRefreshing = isRefreshing,
+                            onRefresh = {
+                                scope.launch {
+                                    isRefreshing = true
+                                    delay(3000L) // Simulated API call
+                                    isRefreshing = false
+                                }
+                            }
+                        )
+                        Button(
+                            onClick = {
+                                isRefreshing = true
+                            },
+                            modifier = Modifier.align(Alignment.BottomCenter)
+                        ) {
+                            Text(text = "Refresh")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeTheme {
+        Greeting("Android")
+    }
+}
